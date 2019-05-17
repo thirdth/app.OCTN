5ad6c18050055a355025bcf01856aab7598a29c0
Zack Glaser <zackglaser@gmail.com>
2019-05-16 22:13:26 -0500

    deleted files

--- functions/garble_cnfg.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../secrets/configOCTN.php'; // database configuration file
-require_once 'functions/getFunctions.php';// all GET Functions
-require_once 'functions/loginFunctions.php';// all Login and register related Functions
-require_once 'functions/insertFunctions.php';// all insert related Functions
-require_once 'functions/updateFunctions.php';// all update related Functions
- ?>
